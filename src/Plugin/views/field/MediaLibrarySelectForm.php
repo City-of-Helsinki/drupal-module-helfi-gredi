1472fb910975f2f52e8d571290f67d7530ce0205
ovidiu <ovidiu.ardelean@ibm.com>
2022-12-09 12:54:37 +0200

    added form for media library view

--- src/Plugin/views/field/MediaLibrarySelectForm.php
+++ src/Plugin/views/field/MediaLibrarySelectForm.php
@@ -9,13 +9,18 @@ use Drupal\acquia_dam\Entity\MediaExpiryDateField;
 use Drupal\acquia_dam\Entity\MediaSourceField;
 use Drupal\Component\Utility\Html;
 use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\CloseDialogCommand;
 use Drupal\Core\Ajax\MessageCommand;
 use Drupal\Core\Ajax\ReplaceCommand;
+use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\media\Entity\Media;
 use Drupal\media_library\MediaLibraryState;
 use Drupal\media_library\Plugin\views\field\MediaLibrarySelectForm as MediaEntityMediaLibrarySelectForm;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Drupal\helfi_gredi_image\Entity\Asset;
 
 /**
  * Media selection field for asset media type.
@@ -35,6 +40,19 @@ final class MediaLibrarySelectForm extends MediaEntityMediaLibrarySelectForm {
    */
   private $entityTypeManager;
 
+  /**
+   * The current user account.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $user;
+
+  /**
+   * The current user account.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
 
   /**
    * {@inheritdoc}
@@ -45,215 +63,167 @@ final class MediaLibrarySelectForm extends MediaEntityMediaLibrarySelectForm {
     return $instance;
   }
 
+
+
   /**
-   * Processes input values and import assets as media if required.
+   * Submit handler for the media library select form.
    *
    * @param array $form
-   *   The form.
+   *   An associative array containing the structure of the form.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The form state.
+   *   The current state of the form.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request.
    *
-   * @phpstan-param array<string, mixed> $form
-   * @see \Drupal\media_library\Form\AddFormBase::processInputValues
+   * @return \Drupal\Core\Ajax\AjaxResponse
+   *   A command to send the selection to the current field widget.
    */
-  public function processInputValues(array $form, FormStateInterface $form_state): void {
-    $media_storage = $this->entityTypeManager->getStorage('media');
-
+  public static function updateWidget(array &$form, FormStateInterface $form_state, Request $request) {
     $field_id = $form_state->getTriggeringElement()['#field_id'];
-    $selected_ids = $form_state->getValue($field_id);
 
-    $selected_ids = $selected_ids ? array_filter(explode(',', $selected_ids)) : [];
+    $selected_ids = $form_state->getValue('media_library_select_form');
 
-    // No IDs were selected, nothing to do.
-    if (count($selected_ids) === 0) {
-      return;
-    }
-    $selected_media_ids = [];
+    $selected_ids = $selected_ids ? array_filter($selected_ids) : [];
+
+    // Fetch selected asset by id.
+    /** @var \Drupal\helfi_gredi_image\Service\GrediDamClient $gredi_client */
+    $gredi_client = \Drupal::service('helfi_gredi_image.dam_client');
+    $asset = $gredi_client->getAsset(current($selected_ids));
+
+    $location = 'public://gredidam';
+
+    /** @var \Drupal\helfi_gredi_image\Service\AssetFileEntityHelper $fileHelper */
+    $fileHelper = \Drupal::service('helfi_gredi_image.asset_file.helper');
+    $file = $fileHelper->createNewFile($asset, $location);
+
+
+//    /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager */
+//    $entityTypeManager = \Drupal::service('entity_type.manager');
+//    /** @var \Drupal\media\MediaTypeInterface $media_type */
+//    $media_type = $entityTypeManager->getStorage('media_type');
+//      ->load($this->configuration['media_type']);
+
+    // Get the source field for this type which stores the asset id.
+//    $source_field = $media_type->getSource()
+//      ->getSourceFieldDefinition($media_type)
+//      ->getName();
 //
-//    // Find existing assets that have been imported, so we do not duplicate the
-//    // asset as a media entity.
-//    // OR we could always duplicate to skip relying on revisions for versions.
-//    $existing_media_query = $media_storage
-//      ->getQuery()
-//      ->accessCheck(FALSE);
-//    // Asset IDs are UUIDs and media IDs are integers. The database engine may
-//    // try to type cast the string to integer. If the UUID begins with a numeric
-//    // value,like "1B4XC", the resulting value will be 1 instead of none.
-//    // @note this does not happen on SQLite but does with MySQL.
-//    $int_selected_ids = array_filter($selected_ids, 'is_numeric');
-//    if (count($int_selected_ids) > 0) {
-//      $existing_media_query
-//        ->condition(
-//          $existing_media_query->orConditionGroup()
-//            ->condition("$source_field_name.asset_id", $selected_ids, 'IN')
-//            ->condition('mid', $int_selected_ids, 'IN')
-//        );
-//    }
-//    else {
-//      $existing_media_query
-//        ->condition("$source_field_name.asset_id", $selected_ids, 'IN');
-//    }
-//    $existing_media_asset_ids = $existing_media_query->execute();
+//    $entity = Media::create([
+//      'bundle' => $media_type->id(),
+//      'uid' => $this->user->id(),
+//      'langcode' => $this->languageManager->getCurrentLanguage()->getId(),
+//      // @todo Find out if we can use status from Gredi DAM.
+//      'status' => 1,
+//      'name' => $asset->name,
+//      'field_media_image' => [
+//        'target_id' => $file->id(),
+//      ],
+//      $source_field => [
+//        'asset_id' => $asset->external_id,
+//      ],
+//      'created' => strtotime($asset->created),
+//      'changed' => strtotime($asset->created),
+//    ]);
 //
-//    /** @var array<int, \Drupal\media\MediaInterface> $existing_media_assets */
-//    $existing_media_assets = $media_storage->loadMultiple($existing_media_asset_ids);
-//    foreach ($existing_media_assets as $existing_media_asset) {
-//      $selected_media_ids[] = $existing_media_asset->id();
-//      if ($existing_media_asset->hasField($source_field_name)) {
-//        $key = array_search($existing_media_asset->get($source_field_name)->asset_id, $selected_ids, TRUE);
-//      }
-//      else {
-//        $key = array_search($existing_media_asset->id(), $selected_ids, TRUE);
-//      }
-//      // Remove this Asset ID from the selected IDs, so that it is not imported.
-//      unset($selected_ids[$key]);
-//    }
+//    $currentLanguage = $this->languageManager->getCurrentLanguage()->getId();
+//    // Check enabled languages.
+//    $siteLanguages = array_keys($this->languageManager->getLanguages());
 //
-//    if (count($selected_ids) > 0) {
-//      $client = $this->clientFactory->getSiteClient();
-//      foreach ($selected_ids as $selected_id) {
-//        try {
-//          $asset = $client->getAsset($selected_id);
-//          $bundle = $this->mediaTypeResolver->resolve($asset);
-//          // Could not resolve to a bundle, which should be impossible.
-//          if ($bundle === NULL) {
-//            continue;
-//          }
-//          $field_values = [
-//            'bundle' => $bundle->id(),
-//            'name' => $asset['filename'],
-//            $source_field_name => [
-//              'asset_id' => $selected_id,
-//            ],
-//            MediaEmbedsField::EMBED_FIELD_NAME => [
-//              'value' => $asset['embeds'],
-//            ],
-//          ];
-//          // Not all asset have an expiration date.
-//          if ($asset['security']['expiration_date']) {
-//            $date = \DateTime::createFromFormat(\DateTimeInterface::ISO8601, $asset['security']['expiration_date']);
-//            $field_values[MediaExpiryDateField::EXPIRY_DATE_FIELD_NAME]['value'] = $date->getTimeStamp();
-//          }
-//          $media = $media_storage->create($field_values);
-//          $media->save();
-//          $selected_media_ids[] = $media->id();
+//    foreach ($asset->keywords as $key => $lang) {
+//      if (in_array($key, $siteLanguages)) {
+//        // For current language case no translation will be added.
+//        if ($key == $currentLanguage) {
+//          $entity->field_media_image = $file->id();
+//          $entity->field_keywords = $asset->keywords[$key];
+//          $entity->field_alt_text = $asset->alt_text[$key];
+//          continue;
 //        }
-//        catch (\Exception $e) {
-//          // Temporarily mark the form as not having completed validation so
-//          // that we can set a new error. This will cause the AJAX callback to
-//          // display the error message.
-//          $form_state->setValidationComplete(FALSE);
-//          $form_state->setError($form, $e->getMessage());
-//          // FormValidator::finalizeValidation converts errors to messages,
-//          // which has already run. We need to manually set the message here.
-//          $this->messenger()->addError('There was an error selecting the asset.');
-//          $this->messenger()->addError($e->getMessage());
-//          $form_state->setValidationComplete();
-//
-//        }
+//        $entity->addTranslation($key, [
+//          'name' => $asset->name,
+//          'field_media_image' => [
+//            'target_id' => $file->id(),
+//          ],
+//          'field_keywords' => (!empty($asset->keywords[$key]) ? $asset->keywords[$key] : ''),
+//          'field_alt_text' => (!empty($asset->alt_text[$key]) ? $asset->alt_text[$key] : ''),
+//        ]);
 //      }
 //    }
-//    $form_state->setValue($field_id, implode(',', $selected_media_ids));
+//
+//    $entity->save();
+
+    // Allow the opener service to handle the selection.
+    $state = MediaLibraryState::fromRequest($request);
+
+    return \Drupal::service('media_library.opener_resolver')
+      ->get($state)
+      ->getSelectionResponse($state, $selected_ids)
+      ->addCommand(new CloseDialogCommand());
   }
 
-//  /**
-//   * {@inheritDoc}
-//   */
-//  public function viewsForm(array &$form, FormStateInterface $form_state) {
-//    $query = $this->view->getRequest()->query->get('media_library_opener_id');
-//    parent::viewsForm($form, $form_state);
-//    $form['#submit'][] = [$this, 'processInputValues'];
-//    $source = $this->view->getRequest()->query->get('source');
-//    if (!$source) {
-//      $allowed_type = array_values($this->view->getRequest()->query->get('media_library_allowed_types'))[0];
-//      $source = $this->entityTypeManager->getStorage('media_type')->load($allowed_type)->getSource()->getPluginDefinition()['provider'];
-//    }
-//    if ($query === 'media_library.opener.editor' && $source === 'helfi_gredi_image') {
-//      // @see \Drupal\layout_builder\Form\ConfigureBlockFormBase::doBuildForm().
-//      // @see https://www.drupal.org/node/2897377.
-//      $form['#id'] = Html::getId($form_state->getBuildInfo()['form_id']);
-//      $form['actions']['submit']['#value'] = $this->t('Next: Select Format');
-//      $form['actions']['submit']['#ajax']['callback'] = [
-//        self::class,
-//        'updateWidgetToNext',
-//      ];
-//    }
-//  }
+  /**
+   * Form constructor for the media library select form.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function viewsForm(array &$form, FormStateInterface $form_state) {
+    $form['#attributes']['class'] = ['js-media-library-views-form'];
 
-//  /**
-//   * Submit handler for media asset dam form.
-//   *
-//   * This handler will take care of moving the form to the next dialog.
-//   *
-//   * @param array $form
-//   *   An associative array containing the structure of the form.
-//   * @param \Drupal\Core\Form\FormStateInterface $form_state
-//   *   The current state of the form.
-//   * @param \Symfony\Component\HttpFoundation\Request $request
-//   *   The current request.
-//   *
-//   * @return \Drupal\Core\Ajax\AjaxResponse
-//   *   A command to send the selection to the current field widget.
-//   */
-//  public static function updateWidgetToNext(array &$form, FormStateInterface $form_state, Request $request): AjaxResponse {
-//    // If we have validation errors, do not process.
-//    // Taken from \Drupal\Core\Ajax\AjaxFormHelperTrait::ajaxSubmit().
-//    if ($form_state->hasAnyErrors()) {
-//      $form['status_messages'] = [
-//        '#type' => 'status_messages',
-//        '#weight' => -1000,
-//      ];
-//      $form['#sorted'] = FALSE;
-//      $response = new AjaxResponse();
-//      $response->addCommand(new ReplaceCommand(
-//        '[data-drupal-selector="' . $form['#attributes']['data-drupal-selector'] . '"]',
-//        $form
-//      ));
-//      return $response;
-//    }
-//
-//    // Logic from updateWidget of Media Library.
-//    $field_id = $form_state->getTriggeringElement()['#field_id'];
-//    $selected_ids = $form_state->getValue($field_id);
-//    $selected_ids = $selected_ids ? array_filter(explode(',', $selected_ids)) : [];
-//
-//    // Allow the opener service to handle the selection.
-//    $state = MediaLibraryState::fromRequest($request);
-//
-//    $current_selection = $form_state->getValue($field_id);
-//    $available_slots = $state->getAvailableSlots();
-//    $selected_count = count(explode(',', $current_selection));
-//    if ($available_slots > 0 && $selected_count > $available_slots) {
-//      $response = new AjaxResponse();
-//      $error = \Drupal::translation()->formatPlural($selected_count - $available_slots, 'There are currently @total items selected, but the maximum number of remaining items for the field is @max. Please remove @count item from the selection.', 'There are currently @total items selected. The maximum number of remaining items for the field is @max. Please remove @count items from the selection.', [
-//        '@total' => $selected_count,
-//        '@max' => $available_slots,
-//      ]);
-//      $response->addCommand(new MessageCommand($error, '#media-library-item-count', ['type' => 'error']));
-//      return $response;
-//    }
-//    return self::buildEmbedForm($form_state, $request, $selected_ids);
-//  }
+    // Add an attribute that identifies the media type displayed in the form.
+    if (isset($this->view->args[0])) {
+      $form['#attributes']['data-drupal-media-type'] = $this->view->args[0];
+    }
 
-//  /**
-//   * Build the embed form for the selected asset.
-//   *
-//   * @param \Drupal\Core\Form\FormStateInterface $form_state
-//   *   The current state of the form.
-//   * @param \Symfony\Component\HttpFoundation\Request $request
-//   *   The current request.
-//   * @param array $selected_ids
-//   *   The array containing the selected assets.
-//   *
-//   * @return \Drupal\Core\Ajax\AjaxResponse|void
-//   *   A command to send the replace the current form with an another one.
-//   */
-//  public static function buildEmbedForm(FormStateInterface $form_state, Request $request, array $selected_ids) {
-//    $asset_type = $request->query->get('media_library_selected_type');
-//    $embed_form = \Drupal::formBuilder()->getForm('Drupal\acquia_dam\Form\EmbedSelectForm', $asset_type, implode(',', $selected_ids));
-//    return (new AjaxResponse())
-//      ->addCommand(new ReplaceCommand('#media-library-wrapper', $embed_form));
-//  }
+    // Render checkboxes for all rows.
+    $form[$this->options['id']]['#tree'] = TRUE;
+    foreach ($this->view->result as $row_index => $row) {
+      $entity = $this->getEntity($row);
+      $form[$this->options['id']][$row_index] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Select @label', [
+          '@label' => $entity->label(),
+        ]),
+        '#title_display' => 'invisible',
+        // TODO change with asset->id
+        '#return_value' => $entity->id(),
+      ];
+    }
+
+    // The selection is persistent across different pages in the media library
+    // and populated via JavaScript.
+    $selection_field_id = $this->options['id'] . '_selection';
+    $form[$selection_field_id] = [
+      '#type' => 'hidden',
+      '#attributes' => [
+        // This is used to identify the hidden field in the form via JavaScript.
+        'id' => 'media-library-modal-selection',
+      ],
+    ];
+
+    // @todo Remove in https://www.drupal.org/project/drupal/issues/2504115
+    // Currently the default URL for all AJAX form elements is the current URL,
+    // not the form action. This causes bugs when this form is rendered from an
+    // AJAX path like /views/ajax, which cannot process AJAX form submits.
+    $query = $this->view->getRequest()->query->all();
+    $query[FormBuilderInterface::AJAX_FORM_REQUEST] = TRUE;
+    $query['views_display_id'] = $this->view->getDisplay()->display['id'];
+    $form['actions']['submit']['#ajax'] = [
+      'url' => Url::fromRoute('media_library.ui'),
+      'options' => [
+        'query' => $query,
+      ],
+      'callback' => [static::class, 'updateWidget'],
+      // The AJAX system automatically moves focus to the first tabbable
+      // element of the modal, so we need to disable refocus on the button.
+      'disable-refocus' => TRUE,
+    ];
+
+    $form['actions']['submit']['#value'] = $this->t('Select Assets');
+    $form['actions']['submit']['#button_type'] = 'primary';
+    $form['actions']['submit']['#field_id'] = $selection_field_id;
+  }
 
   /**
    * {@inheritdoc}
