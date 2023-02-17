2aa42920e9fa416f50ef82847bba55e8e539ad41
ovidiu <ovidiu.ardelean@ibm.com>
2023-02-17 14:43:01 +0200

    media library integration test

---
+++ tests/modules/helfi_gredi_test/src/GrediClientTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\helfi_gredi_test;
+
+use Drupal\Component\Serialization\Json;
+use Drupal\helfi_gredi\GrediClient;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+
+class GrediClientTest extends GrediClient {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function apiCallGet($apiUri, $queryParams = []) : ResponseInterface {
+
+    return new Response(200, ['Content-Type' => 'application/json'],
+      file_get_contents(__DIR__ . sprintf('/../../../fixtures/assetData_%s.json', 'all')));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getAssetData(string $id): array | NULL {
+   return Json::decode(file_get_contents(__DIR__ . sprintf('/../../../fixtures/assetData_%s.json', 'all')));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFileContent($assetId, $downloadUrl) : FALSE|string {
+    return file_get_contents(__DIR__ . sprintf('/../../../fixtures/assetData_%s.json', 'all'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMetaFields(): array {
+    return Json::decode(file_get_contents(__DIR__ . sprintf('/../../../fixtures/assetData_%s.json', 'metafields')));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function searchAssets($search = '', $sortBy = '', $sortOrder = '', $limit = 10, $offset = 0): array {
+    $items = Json::decode(
+      file_get_contents(__DIR__ . sprintf('/../../../fixtures/assetData_%s.json', 'all'))
+    );
+    $result = [];
+    foreach ($items as $item) {
+      $asset = (array) $item;
+      $result[] = $asset;
+    }
+
+    return $result;
+  }
+
+}
--- tests/src/FunctionalJavascript/GrediFunctionalTest.php
+++ tests/src/FunctionalJavascript/GrediFunctionalTest.php
@@ -4,15 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\helfi_gredi\FunctionalJavascript;
 
-use Behat\Mink\Element\NodeElement;
-use Drupal\Component\Serialization\Json;
-use Drupal\Core\File\FileSystemInterface;
-use Drupal\file\Entity\File;
-use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
-use Drupal\helfi_gredi\GrediClient;
-use Drupal\media\Entity\Media;
 use Drupal\Tests\field\Traits\EntityReferenceTestTrait;
-use Drupal\Tests\helfi_gredi\Traits\GrediClientTestTrait;
 use Drupal\Tests\media_library\FunctionalJavascript\MediaLibraryTestBase;
 use PHPUnit\Runner\BaseTestRunner;
 
@@ -29,7 +21,6 @@ use PHPUnit\Runner\BaseTestRunner;
 class GrediFunctionalTest extends MediaLibraryTestBase {
 
   use EntityReferenceTestTrait;
-  use GrediClientTestTrait;
 
   /**
    * {@inheritdoc}
@@ -53,20 +44,21 @@ class GrediFunctionalTest extends MediaLibraryTestBase {
     'block',
     // Install dblog to assist with debugging.
     'dblog',
+    'views_remote_data',
     'helfi_gredi',
+    'helfi_gredi_test'
   ];
 
   /**
    * {@inheritdoc}
    */
   protected function setUp(): void {
-    parent::setUp();
 
+    parent::setUp();
     $this->container->get('config.installer')->installDefaultConfig('module', 'helfi_gredi');
 
-    $this->createServiceMocks();
-
     $this->drupalCreateContentType(['type' => 'page']);
+
 
     $this->createEntityReferenceField(
       'node',
@@ -77,7 +69,6 @@ class GrediFunctionalTest extends MediaLibraryTestBase {
       'default',
       [
         'target_bundles' => [
-          'image',
           'gredi_asset'
         ],
       ],
@@ -89,9 +80,8 @@ class GrediFunctionalTest extends MediaLibraryTestBase {
         'region' => 'content',
         'settings' => [
           'media_types' => [
-            'image',
-            'gredi_asset',
-          ]
+            'gredi_asset'
+            ]
         ]
       ])
       ->save();
@@ -102,22 +92,22 @@ class GrediFunctionalTest extends MediaLibraryTestBase {
       ->save();
   }
 
-//  /**
-//   * {@inheritdoc}
-//   */
-//  protected function tearDown(): void {
-//    $status = $this->getStatus();
-//
-//    if ($status === BaseTestRunner::STATUS_ERROR || $status === BaseTestRunner::STATUS_WARNING || $status === BaseTestRunner::STATUS_FAILURE) {
-//      $log = \Drupal::database()
-//        ->select('watchdog', 'w')
-//        ->fields('w')
-//        ->execute()
-//        ->fetchAll();
-//      throw new \RuntimeException(var_export($log, TRUE));
-//    }
-//    parent::tearDown();
-//  }
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown(): void {
+      $status = $this->getStatus();
+
+      if ($status === BaseTestRunner::STATUS_ERROR || $status === BaseTestRunner::STATUS_WARNING || $status === BaseTestRunner::STATUS_FAILURE) {
+        $log = \Drupal::database()
+          ->select('watchdog', 'w')
+          ->fields('w')
+          ->execute()
+          ->fetchAll();
+        throw new \RuntimeException(var_export($log, TRUE));
+      }
+      parent::tearDown();
+    }
 
   /**
    * Test media library integration with gredi module.
@@ -142,31 +132,27 @@ class GrediFunctionalTest extends MediaLibraryTestBase {
     $this->drupalGet('/node/add/page');
     $this->assertSession()->responseContains('Create page');
 
-    $grediClient = new GrediClient(
-      $this->guzzleClientMock,
-      $this->configFactoryMock,
-      $this->authServiceMock,
-      $this->loggerChannelFactoryMock,
-      $this->cacheBin
-    );
-
-    $this->container->set('helfi_gredi.dam_client', $grediClient);
-
-    $this->setApiResponse('1');
-
+    // Open media library and assert the media items are visible.
     $this->click('#edit-media-field-open-button');
+    $this->assertSession()->waitForElement('css', '#media-library-widget');
+    $this->assertSession()->pageTextContains('Add or select media');
+    $this->assertSession()->pageTextContains('test1.jpg');
+    $this->assertSession()->pageTextContains('test2.png');
+    $this->assertSession()->pageTextContains('test3.jpg');
+    $this->assertSession()->pageTextContains('test4.jpg');
 
-    $modal = $this->assertSession()->waitForElement('css', '#media-library-widget');
+    // Select a media.
+    $this->click('input[type="checkbox"][name="media_library_select_form[1]"]');
+    $this->assertSession()->checkboxChecked('media_library_select_form[1]');
+    $this->assertSession()->elementExists('css', '.ui-dialog-buttonpane')->pressButton('Insert selected');
 
-//    $tabs = $this->getSession()
-//      ->getPage()
-//      ->findAll('css', '.media-library-menu__link');
-//    self::assertSame([
-//      'Gredi Image',
-//      'Image',
-//    ], array_map(static function (NodeElement $element) {
-//      return $element->getText();
-//    }, $tabs));
+    // Assert the media was selected.
+    $this->assertSession()->waitForElement('css', 'media-library-item__preview-wrapper');
+    $this->assertSession()->pageTextContains('test2.png');
+
+    // Assert the media was created.
+    $this->drupalGet('/admin/content/media');
+    $this->assertSession()->pageTextContains('test2.png');
 
   }
 
